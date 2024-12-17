1dee22b31119c7a8edcfe644ac017ce234d8195a
alex-alm66 <afa66@georgetown.edu>
2024-12-17 15:24:32 -0500

    Updated ff tests

--- test/ff_test.py
+++ test/ff_test.py
@@ -6,12 +6,12 @@ def test_ff_data_fetching():
 
     data = fetch_ff_json(1)
     assert isinstance(data,dict)
-    assert len(data['body']['teamDefenseProjections']) == 32
-    assert len(data['body']['playerProjections']) > 300
+    assert len(data['teamDefenseProjections']) == 32
+    assert len(data['playerProjections']) > 300
 
-    players = data['body']['playerProjections']
+    players = data['playerProjections']
     assert isinstance(players,dict)
 
-    teams = data['body']['teamDefenseProjections']
+    teams = data['teamDefenseProjections']
     assert isinstance(teams,dict)
     
